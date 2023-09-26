4b5b0f0a538ebca6c0bca0509e8b7b2f8f85207f
mochu2010@gmail.com <mochu2010@gmail.com>
2023-09-26 08:10:13 +0700

    tugas-python_kondisi if

---
+++ logika_kondisi.py
@@ -0,0 +1,13 @@
+print("ini program logika kondisi")
+
+nilai=91
+
+if (nilai<70):
+	print("Kurang")
+elif (nilai>70 and nilai<80 ):
+	print("cukup")
+elif (nilai>80 and nilai<90):
+	print("baik")
+else:
+	print("sangat baik")
+
