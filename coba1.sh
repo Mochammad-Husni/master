dab1f5d1428fd021edd2cd516dfa948cb81772de
Your Name <you@example.com>
2023-09-29 07:20:12 +0700

    tambah file

---
+++ coba1.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+echo -n " selamat  datang " 
+read nama
+
+if [ $nama -eq whoami ]
+then 
+ echo " Selamat Datang $nama "
+
+else
+
+echo " Anda siapa "
+
+df
+fi
+
+
+
---
+++ ulang.py
@@ -0,0 +1,7 @@
+i=1
+while (i < 5):
+ do
+  print(i)
+    i +=1
+ done
+
